802df2c2279fba727b9fb8f67bd20cb61ffb8d39
WayuSuradech <wayu.suradech.k@gmail.com>
2025-03-13 16:08:55 +0700

    Now is Attract!!

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class Gravity : MonoBehaviour
 {
     private Rigidbody rb;
-    private const float G = 0.006674f;
+    private const float G = 0.00667f;
     public static List<Gravity> OtherObjectsList;
+    
 
     //[SerializeField] private bool planet = false;
     //[SerializeField] private int orbitSpeed = 1000;
@@ -24,15 +25,23 @@ public class Gravity : MonoBehaviour
             //rb.AddForce(Vector3.left * orbitSpeed);
         }
     }
-
     private void FixedUpdate()
     {
-        
+        foreach (var obj  in OtherObjectsList)
+        {
+            if (obj != this)
+            {
+                Attract(obj); 
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    void Attract(Gravity other)
     {
-        
+        Rigidbody otherRb = other.rb;
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        float forceMagnitude = G * (rb.mass * otherRb.mass/ Mathf.Pow(distance, 2));
+        Vector3 gravityForce = forceMagnitude * direction.normalized;
+        otherRb.AddForce(gravityForce);
     }
 }
